a412fcb4e57cbb81996347bafbad14c44dcd5622
VinLvy <davinputrafibrian24@gmail.com>
2024-11-11 18:00:30 +0700

    Install html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
-// webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  mode: 'development',
-  devServer: {
-    static: './dist',
-    open: true,
-  },
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    mode: 'development',
+    devServer: {
+        static: './dist',
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
 };
