bb95f1fc30c0239017643c7cf4cf9ca846eb49b3
VinLvy <davinputrafibrian24@gmail.com>
2024-11-14 19:54:37 +0700

    Main code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-console.log("Webpack is working!");
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+        this.notes = "";
+        this.checklist = [];
+    }
+}
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.todos = [];
+    }
+}
+
+const ProjectManager = (() => {
+    const projects = [];
+    const addProject = (name) => {
+        projects.push(new Project(name));
+        saveToLocalStorage();
+    };
+    const addTodoToProject = (projectName, todo) => {
+        const project = projects.find(p => p.name === projectName);
+        if (project) {
+            project.todos.push(todo);
+            saveToLocalStorage();
+        }
+    };
+    return { projects, addProject, addTodoToProject };
+})();
+
+const saveToLocalStorage = () => {
+    localStorage.setItem("projects", JSON.stringify(ProjectManager.projects));
+};
+
+const loadFromLocalStorage = () => {
+    const savedProjects = JSON.parse(localStorage.getItem("projects"));
+    if (savedProjects) {
+        ProjectManager.projects = savedProjects.map(projData => {
+            const project = new Project(projData.name);
+            project.todos = projData.todos.map(todoData => Object.assign(new Todo(), todoData));
+            return project;
+        });
+    }
+};
